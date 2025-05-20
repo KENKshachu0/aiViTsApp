91828fe08f9a74a6e52209cbea08b01771621948
KENK <ken1178776360@icloud.com>
2025-05-20 11:38:13 +0800

    webSocket first done

--- pages/asr/index.vue
+++ pages/asr/index.vue
@@ -1,35 +1,76 @@
 <template>
   <view class="container">
     <button @click="startASR">🎤 开始语音识别</button>
+    <button @click="stopASR">⏹️ 停止</button>
     <view class="result">识别结果：{{ result }}</view>
   </view>
 </template>
 
 <script>
-import AliASR from '@/utils/nls.js'
+import NLSClient from '@/utils/nls.js'
 
 export default {
   data() {
     return {
       result: '',
-      asr: null,
+      nls: null,
+      recorder: null
     }
   },
   methods: {
     startASR() {
-      const appkey = '2BBIDJhM2wHRBQIl'// aliyun appkey    STATIC!!!
-      const token = 'a5ef6be6b8424265bf8b746917c327c3' //aliyun token
-
-      this.result = ''
-      this.asr = new AliASR({
-        appkey,
-        token,
-        onResult: (res) => {
-          this.result = res
-          console.log("识别结果：", res)
+      this.nls = new NLSClient({
+        appkey: '2BBIDJhM2wHRBQIl', //static！！！
+        token: 'b76f0ff2634b458a9d1580fa4fd5bef0',
+        onMessage: (msg) => {
+          if (msg.result && msg.result.text) {
+            this.result += msg.result.text
+          }
+        },
+        onOpen: () => {
+          console.log('WebSocket已连接')
+          this.startRecorder()
+        },
+        onError: (err) => {
+          console.error('WebSocket出错', err)
+        },
+        onClose: () => {
+          console.log('连接已关闭')
         }
       })
-      this.asr.start()
+      this.result = ''
+      this.nls.connect()
+    },
+    stopASR() {
+      this.stopRecorder()
+      this.nls && this.nls.stop()
+    },
+    startRecorder() {
+      const recorder = uni.getRecorderManager()
+      this.recorder = recorder
+      recorder.onFrameRecorded((res) => {
+        // 每次录音帧（默认为 20ms）发送给阿里云识别服务
+        this.nls.send(res.frameBuffer)
+      })
+      recorder.onStart(() => {
+        console.log('录音开始')
+      })
+      recorder.onError((err) => {
+        console.error('录音错误', err)
+      })
+      recorder.start({
+        format: 'pcm',
+        sampleRate: 16000,
+        numberOfChannels: 1,
+        encodeBitRate: 256000,
+        frameSize: 1 // 单位 KB，表示每帧大小
+      })
+    },
+    stopRecorder() {
+      if (this.recorder) {
+        this.recorder.stop()
+        this.recorder = null
+      }
     }
   }
 }
